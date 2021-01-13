05d8419417501b8a88915b4e8813e93f29dfa064
CyberNet-Git <v.v.panfilov@gmail.com>
2021-01-13 23:28:54 +0700

    Sprite module ready. Next to load new game images

--- Sprite.py
+++ Sprite.py
@@ -1,16 +1,8 @@
 import os
 import pygame
-from Tilemap import Tilemap
+
 import const
 
-
-"""
-Спрайты:
-1. должны ЛИ? уметь себя рисовать на указанной поверхности
-2. должны возвращать готовые ссылки с нужным разрешением (генерировать и кэшировать или брать подготовленные?)
-3. текущее разрешение карты брать у GameEngine
-
-"""
 class Sprite:
 
     def __init__(self, *args, **kwargs):
@@ -66,6 +58,13 @@ class SquareMultiSprite(Sprite):
         self.cols = self.size[0] // size
         self.rows = self.size[1] // size
 
+    def clear_cache(self):
+        self.size = self._image.get_size()
+        cache_index = ((self.sprite_size,) + self.active_sprite)
+        rect = (0, 0, self.sprite_size, self.sprite_size) # top left sprite is always default active
+        img = self._image.subsurface(rect)
+        self.cache= {cache_index: img}
+
     def get_sprite(self, size=None):
         size = size or self.sprite_size
         cache_index = ((size,) + self.active_sprite)
@@ -76,13 +75,6 @@ class SquareMultiSprite(Sprite):
             self.cache[cache_index] = pygame.transform.scale(img, (size, size))
         return self.cache[cache_index]
 
-    def clear_cache(self):
-        self.size = self._image.get_size()
-        cache_index = ((self.sprite_size,) + self.active_sprite)
-        rect = (0, 0, self.sprite_size, self.sprite_size) # top left sprite is always default active
-        img = self._image.subsurface(rect)
-        self.cache= {cache_index: img}
-
     def set_active_sprite(self, active_sprite=None):
         if active_sprite is None:
             active_sprite = (0,0)
@@ -91,13 +83,27 @@ class SquareMultiSprite(Sprite):
         self.active_sprite = active_sprite
 
 class AnimatedSprite(SquareMultiSprite):
-    ROUNDTRIP=0
-    CIRCLE=1
+    ROUNDTRIP = 0
+    CIRCLE = 1
+    TIMER = 0
+    MANUAL = 1
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.mode = self.ROUNDTRIP
+        self.animation = self.TIMER
+        self.timer = time.monotonic()
         self.step = 1
+
+    def get_sprite(self, size=None):
+        sprite = super().get_sprite(size)
+        if self.animation == self.MANUAL:
+            self.next_frame()
+        elif self.animation == self.TIMER \
+                and time.monotonic() - self.timer > 1/5: # hardcoded 0.2 sec timeframe
+            self.next_frame()
+            self.timer = time.monotonic()
+        return sprite
 
     def next_frame(self):
         col = self.active_sprite[1] + self.step
@@ -109,132 +115,181 @@ class AnimatedSprite(SquareMultiSprite):
         self.set_active_sprite((self.active_sprite[0], col))
 
     def set_view(self, view=0):
-        view = view % (self.rows-1)
+        view = view % (self.rows)
         self.set_active_sprite((view, self.active_sprite[1]))
-
-    def get_sprite(self, size=None):
-        sprite = super().get_sprite(size)
-        self.next_frame()
-        return sprite
-        
 
 
 class SpriteProvider:
+    """
+        Класс обеспечивает игру всеми необходимыми графическими примитивами (спрайтаими)
+    все они умеют по запросу вернуть объект pygame.Surface() для отрисовки образа
+    нужного объекта или ячейки карты.
+
+        У меня тут в модуле определено несколько типов спрайтов:
+
+        Sprite - обычный спрайт из графического файла с произвольными размерами 
+    по ширине и высоте.
+
+        SquareSprite - спрайт, у которого высота и ширина равны, т.е. квадратный. 
+    Если картинка не квадратная, то меньшую сторону растянем при загрузке.
+
+        SuareMultiSprite - это такой набор спрайтов в одной картинке в виде сетки 
+    с заданным размером квадрата. Подразумевается, что файл корректный, 
+    иначе изображения съедут.
+
+        AnimatedSprite - такой особый вид спрайта, который умеет меняться во времени 
+    или по запросу. Содержит набор кадров. По строкам - вид (например, 
+    спереди, справа,...) По столбцам - раскадровка движения.
+
+        Иерархию наследования можно увидеть из исходного кода выше.
+    """
     GRASS = 0
     WALL = 1
     FLOOR = 2
     BORDER = 3
     CHEST = 4
     STAIRS = 5
+    
+    NEW = True
+    OLD = False
 
     def __init__(self, size=const.DEFAULT_SPRITE_SIZE):
         self.size = size
-        #self.static = Tilemap(32, STATIC_TEXTURES) # Тут указываем реальный размер плитки в картинке (32)
-        #self.hero = Tilemap(32, HERO_TILEMAP)
-        self.ugly_images = True
+        self.look = False
         self.sprites = {
-            True: {'hero':{}, 'map':{}, 'objects':{}, 'enemies':{}, 'ally':{}}, # tilemaps
-            False:{'hero':{}, 'map':{}, 'objects':{}, 'enemies':{}, 'ally':{}} # old sprites
+            self.NEW: {'hero':{}, 'map':{}, 'objects':{}, 'enemies':{}, 'ally':{}}, # tilemaps
+            self.OLD:{'hero':{}, 'map':{}, 'objects':{}, 'enemies':{}, 'ally':{}} # old sprites
         }
     
     def load_ugly_sprites(self, objects):
-        #FIXME загрузить тут убогие спрайты из шаблона проекта
+        #FIXME загрузить тут убогие спрайты из шаблона проекта чтобы только показать, что мы это могём
+        # - не могём, а мОгем, -- (С) Маэстро. "В бой идут одни старики" :)
         self.objects = objects
-        rows = 10
-        cols = 8
-        for o in objects: cols = max(len(objects[o]), cols)
         
-        _map = Tilemap(32, rows = rows, cols = cols)
-        
-        tile = _map.batch_load_tile_at(self.WALL, 0, os.path.join("texture","map", "wall.png"))
-        [_map.batch_add_tile_at(self.WALL, i + 1, t) for i, t in enumerate([tile] * (cols - 1))]
-        [_map.batch_add_tile_at(self.BORDER, i, t) for i, t in enumerate([tile] * cols)]
-        g1 = _map.batch_load_tile_at(self.FLOOR, 0, os.path.join("texture","map", "Ground_1.png"))
-        g2 = _map.batch_load_tile_at(self.FLOOR, 1, os.path.join("texture","map", "Ground_2.png"))
-        g3 = _map.batch_load_tile_at(self.FLOOR, 2, os.path.join("texture","map", "Ground_3.png"))
-        [_map.batch_add_tile_at(self.GRASS, i, t) for i, t in enumerate([g1, g2, g3] * 2 + [g1, g2] )]
-        _map.update_tilemaps()
-        #self.ugly['map'] = _map 
+        # Герой + Внешний вид (с мечом, копьем, эффектом) + направление взгляда
+        # Карта + Тип (трава, стена, вода, пол) + внешний вид
+        # Объекты + Имя объекта (сундук, лестница) + состояние (закрыт, открыт)
+        # Враги + Имя объекта (он же внешний вид) + направление взгляда
+        # Союзники + Имя объекта (он же внешний вид) + направление взгляда
+        self.sprites[self.OLD]['hero'][None] = {None: Sprite(os.path.join("texture","hero", "Hero.png"))}
+        self.sprites[self.OLD]['map']['wall'] = {None: Sprite(os.path.join("texture","map", "wall.png"))}
+        self.sprites[self.OLD]['map']['floor'] = {}
+        self.sprites[self.OLD]['map']['floor'][0] = Sprite(os.path.join("texture","map", "Ground_1.png"))
+        self.sprites[self.OLD]['map']['floor'][1] = Sprite(os.path.join("texture","map", "Ground_2.png"))
+        self.sprites[self.OLD]['map']['floor'][2] = Sprite(os.path.join("texture","map", "Ground_3.png"))
 
-        def pack_sprites(obj_type):
-            self.ugly[obj_type] = Tilemap(32, rows = 1, cols = max(5,len(objects[obj_type])))
-            self.ugly_ids[obj_type] = {}
-            objs = objects[obj_type]
-            for i, name in enumerate(objs):
-                print('pack', i, objs[name])
-                self.ugly[obj_type].batch_load_tile_at(0, i, os.path.join("texture","map", obj_type, objs[name]['sprite'][0]))
-                self.ugly_ids[obj_type][name] = i
-            self.ugly[obj_type].update_tilemaps()
-        
-        # pack single sprites or list to tilemap 
-        for obj_class in objects:
-            pack_sprites(obj_class)
+        # загрузить спрайты объектов из YAML
+        for t in objects: # [objects, enemies, ally]
+            for name in objects[t]:
+                # t - тип (ally, enemies) + objects[t] - Имя + состояние - всегда неопределено
+                filename = objects[t][name]['sprite'][0]
+                self.sprites[self.OLD][t][name] = {None: Sprite(os.path.join("texture", t, filename ))}
 
-        self.ugly['hero'] = Tilemap(32, os.path.join("texture", "Hero.png"))
-
+    def load_beauty_sprites(self):
+        # load Hero
+        # load Map
+        # load Objects
+        # load Ally
+        # load Enemies
         pass
-    
-    def get_static(self, what, num, size):
-        if self.ugly_images:
-            return self.ugly['map'].get_sprite(what, num, size)
-        else:
-            return self.static.get_sprite(what, num, size)
 
-    def get_grass(self, num):
-        return self.static.get_sprite(SpriteProvider.GRASS, num, self.size)
-
-    def get_hero(self, sprite_size):
-        if self.ugly_images:
-            return self.ugly['hero'].get_sprite(0, 0, sprite_size)
-        else:
-            return self.hero.get_sprite(2, 2, sprite_size)
-
-    def get_object(self, obj_type, name, sprite_size):
-        if self.ugly_images:
-            print('get object: ', obj_type, name)
-            return self.ugly[obj_type].get_sprite(0, self.ugly_ids[obj_type][name], sprite_size)
-        else:
-            #TODO возвращать красивых монстриков.
-            return self.ugly[obj_type].get_sprite(0, self.ugly_ids[obj], sprite_size)
-            pass
-    
-    def get_sprite(what, name, view, phase):
-        return self.sprites[self.ugly][what][name][view][phase].get_sprite(self.size)
+    def get_sprite(self, what, name, view):
+        try:
+            return self.sprites[self.look][what][name][view]
+        except:
+            #FIXME нужно вернуть любой подходящий спрайт
+            return None
 
 
 if __name__=='__main__':
-    from copy import deepcopy
+
+    import time, yaml
+
     pygame.init()
     screen = pygame.display.set_mode((640, 640))
-    floor = SquareMultiSprite(filename="texture\\map\\map.png")
-    sprite = AnimatedSprite(filename="texture\\hero\\Soldier 02-1.png")
-    chest = floor
-    tiles = Tilemap(32, rows=10, cols=8)
     screen.fill((255, 255, 255))
-    s = 64
-    spr = sprite.get_sprite(s)
-    sprite.set_active_sprite((2, 0))
-    print(spr.get_size())
-    import time
-    t1 = time.monotonic()
-    walk_circle = [0,1,2,1]
-    for i in range(20):
+
+    def ugly(s, sp):
+        r = 0
+        for i, t in enumerate(sp.sprites[False]):
+            c = 4
+            for j, name in enumerate(sp.sprites[False][t]):
+                for k, view in enumerate(sp.sprites[False][t][name]):
+                    sprite = sp.get_sprite(t, name, view)
+                    screen.blit(sprite.get_sprite(s), ((c+k)*s, r*s))
+                c += 1
+            r += 1
+
+    def anim(s):
+        #walk_circle = [0,1,2,1]
         for r in range(4):
             for c in range(1):
                 #sprite.set_active_sprite((1, walk_circle[i % 4]))
-                floor.set_active_sprite((2,r+4))
-                screen.blit(floor.get_sprite(s), (c*s, r*s))
-                screen.blit(floor.get_sprite(s), ((c+1) * s, r*s))
-                chest.set_active_sprite((6,r))
+                for f in range(floor.cols):
+                    floor.set_active_sprite((const.FLOOR,f))
+                    screen.blit(floor.get_sprite(s), ((c+f) * s, (r)*s))
+                chest.set_active_sprite((5,r+4))
                 screen.blit(chest.get_sprite(s), ((c+1) * s , r*s))
-                sprite.set_view(r)
+                sprite.set_view( r + view )
                 screen.blit(sprite.get_sprite(s), (c*s, r*s))
+        ugly(sprite_size, sp)
         pygame.display.flip()
-        time.sleep(1/5)
-        sprite.next_frame()
-        screen.fill((255, 255, 255))
+        #sprite.next_frame()
+        #screen.fill((255, 255, 255))
+
+    with open("objects.yml", "r") as file:
+        objects = yaml.load(file.read())
+
+    sp = SpriteProvider()
+    sp.load_ugly_sprites(objects)
+    sprite_size = 64
+
+    
+    t1 = ltime = rtime = time.monotonic()
+    ldown = rdown = False
+    floor = SquareMultiSprite(filename="texture\\map\\map.png")
+    sprite = AnimatedSprite(filename="texture\\hero\\Soldier 02-1.png")
+    chest = floor
+    sprite.set_active_sprite((2, 0))
+    sprite.animation = AnimatedSprite.TIMER
+    shift = 0
+    view = 0
+    while True:
+        if pygame.event.peek(eventtype=[pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP], pump=True):
+            
+            for event in pygame.event.get(eventtype=[pygame.QUIT, pygame.KEYDOWN, pygame.KEYUP]):
+                if event.type == pygame.QUIT:
+                    exit(0)
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        ldown = True
+                        shift += 1
+                        view = [const.FRONT, const.LEFT, const.BACK, const.RIGHT][shift % 4]
+                        ltime = time.monotonic()
+                    if event.key == pygame.K_RIGHT:
+                        rdown = True
+                        shift -= 1
+                        view = [const.FRONT, const.LEFT, const.BACK, const.RIGHT][shift % 4]
+                        rtime = time.monotonic()
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_ESCAPE:
+                        exit(0)
+                    if event.key == pygame.K_LEFT:
+                        ldown = False
+                    if event.key == pygame.K_RIGHT:
+                        rdown = False
+        t = time.monotonic()
+        if  t - ltime > 1/5 and ldown: 
+            ltime = t
+            shift += 1
+            view = [const.FRONT, const.LEFT, const.BACK, const.RIGHT][shift % 4]
+        if  t - rtime > 1/5 and rdown: 
+            rtime = t
+            shift -= 1
+            view = [const.FRONT, const.LEFT, const.BACK, const.RIGHT][shift % 4]
+
+        anim(sprite_size)
+
     print ("{}".format(time.monotonic() - t1))
 
-    #screen.blit(tiles.tilemaps[32]['img'], (0,0))
-    while pygame.event.wait().type != pygame.locals.QUIT:
-        pass
